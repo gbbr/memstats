c9512d36bef1c58b8f0fbec91301659630bac3ba
gbbr <ga@stripetree.co.uk>
2014-11-17 00:49:33 +0000

    example

--- memstats.go
+++ memstats.go
@@ -2,6 +2,7 @@
 // Collector information, run stack traces and memory profiles. The default values are
 // configurable via the options provided by the API. To run the server, place this command
 // at the top of your application:
+//
 // Example:
 // 	go memstats.Serve()
 // By default, the memory profile will be viewable on HTTP port :6061
