53f6eb67c02753d5ceb402676f3e34c7ed4c0f82
gbbr <ga@stripetree.co.uk>
2014-11-17 13:21:21 +0000

    Load templates only once

--- cmd/memstats/memstats.go
+++ cmd/memstats/memstats.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"html/template"
 	"log"
 	"net"
 	"net/http"
@@ -15,16 +16,13 @@ var (
 	laddr = flag.String("http", ":8080", "HTTP address to listen on")
 	// saddr is the address the HTTP page will attempt to connect to via websockets.
 	saddr = flag.String("sock", "localhost:6061", "Adress the WebSockets listen on.")
+	// web templates
+	tpl *template.Template
 )
 
 // serveHTTP serves the front-end HTML/JS viewer
 func serveHTTP(w http.ResponseWriter, req *http.Request) {
-	t, err := web.Template()
-	if err != nil {
-		fmt.Fprintf(w, "Error parsing template: %s", err)
-		return
-	}
-	if err := t.ExecuteTemplate(w, "main", *saddr); err != nil {
+	if err := tpl.ExecuteTemplate(w, "main", *saddr); err != nil {
 		fmt.Fprintf(w, "Error parsing template: %s", err)
 	}
 }
@@ -34,6 +32,10 @@ func main() {
 	hst, _, err := net.SplitHostPort(*saddr)
 	if len(hst) == 0 || err != nil {
 		log.Fatal("sockaddr must be host[:port]. ERR: %s", err)
+	}
+	tpl, err = web.Template()
+	if err != nil {
+		log.Fatal(err)
 	}
 	http.HandleFunc("/", serveHTTP)
 	err = http.ListenAndServe(*laddr, nil)
